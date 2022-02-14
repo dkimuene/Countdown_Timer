a69020975b44955b7c197832b8ed4afe47b21eab
Deodat Kimuene <dkimuene1@gmail.com>
2022-02-13 20:56:20 -0600

    Added Look and Style of Counter
    Added a class to the text od the days, hours ,minutes, and seconds. Added the function countdown to calculate the days, hours, minutes, seconds of a date.

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,20 @@
 function init() {
+    const start = "2022-02-24 17:30:00"
+
+    function countdown () {
+        const startDate = new Date(start);
+        const currentDate = new Date();
+        let diff = startDate - currentDate
+        let inToSeconds = diff / 1000;
+        let days = Math.floor(inToSeconds /3600/ 24);
+        let hours = Math.floor(inToSeconds /3600) % 24;
+        let minites = Math.floor(inToSeconds / 60) % 60;
+        let seconds = Math.floor(inToSeconds) % 60;
+
+        console.log(days, hours, minites, seconds);
+    }
+    countdown();
+    setInterval(countdown, 1000);
     
 }
 
