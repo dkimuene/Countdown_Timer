8b33adf7475efe2ba553fcf787622d6b306231d3
Deodat Kimuene <dkimuene1@gmail.com>
2022-02-13 22:54:37 -0600

    Added Countdown
    Added the finished countdown function that finds the days, hours, minutes, seconds to the event selected. Added a format function to have hours, minutes, seconds to all be two digits.

--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,35 @@
 function init() {
-    const start = "2022-02-24 17:30:00"
+    const start = "2022-02-24 17:30:00";
+    const daysElement = document.getElementById("days");
+    const hoursElement = document.getElementById("hours");
+    const minutesElement = document.getElementById("minutes");
+    const secondsElement = document.getElementById("seconds");
 
     function countdown () {
         const startDate = new Date(start);
         const currentDate = new Date();
         let diff = startDate - currentDate
+        
         let inToSeconds = diff / 1000;
+
         let days = Math.floor(inToSeconds /3600/ 24);
         let hours = Math.floor(inToSeconds /3600) % 24;
-        let minites = Math.floor(inToSeconds / 60) % 60;
+        let minutes = Math.floor(inToSeconds / 60) % 60;
         let seconds = Math.floor(inToSeconds) % 60;
 
-        console.log(days, hours, minites, seconds);
+        daysElement.innerHTML = days;
+        hoursElement.innerHTML = format(hours);
+        minutesElement.innerHTML = format(minutes);
+        secondsElement.innerHTML = format(seconds);
     }
-    countdown();
+    function format(time){
+        if(time < 10){
+            return `0${time}`
+        } else{ 
+            return time
+        }
+
+    }
     setInterval(countdown, 1000);
     
 }
